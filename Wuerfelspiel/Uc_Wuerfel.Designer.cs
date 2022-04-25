83c580423120c6a294c95d27918ebf373fc84ddf
Yassine <yassine.h2@gso,schule.koeln>
2022-04-25 12:10:23 +0200

    yeah

---
+++ Wuerfelspiel/Uc_Wuerfel.Designer.cs
@@ -0,0 +1,47 @@
+﻿
+namespace Wuerfelspiel
+{
+    partial class Uc_Wuerfel
+    {
+        /// <summary> 
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Uc_Wuerfel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "Uc_Wuerfel";
+            this.Size = new System.Drawing.Size(249, 232);
+            this.Load += new System.EventHandler(this.Uc_Wuerfel_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Wuerfelspiel/Uc_Wuerfel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wuerfelspiel
+{
+    public partial class Uc_Wuerfel : UserControl
+    {
+
+        int W 
+        public Uc_Wuerfel()
+        {
+            InitializeComponent();
+            W = new Wuerfel();
+        }
+
+        private void Uc_Wuerfel_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
